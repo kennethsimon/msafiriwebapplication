73d7801698c9bc5cf6c25aaeb0693fd71cf27db9
kennethsimon <kenethsimon@gmail.com>
2019-01-26 20:58:27 +0300

    restructure
    website restructure

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 $(document).ready(function(){
+    $('.sidenav').sidenav();
     $('.tabs').tabs();
-    $('.owl-carousel').owlCarousel({
-        items:3,
-        margin: 25,
-        autoHeight:true,
-        loop: true,
-        nav:true,
-        autoplay: true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:true,
-                loop:false
-            }
-        }
-    });
+    $('.collapsible').collapsible();
+    $('select').formSelect();
 });
